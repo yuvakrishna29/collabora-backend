0361e9e547308bd1dd546e7358b90a104c6794de
Vinayvinuus <gaddamvinay450@gmail.com>
2025-09-02 13:12:46 +0530

    R&D-421 | added the index.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let express = require('express');
 let path = require('path');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
+let indexRouter = require('./routes/index');
 let wopiRouter = require('./routes/wopi');
 
 // maximum request body size handled by the bodyParser package
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.raw({limit: maxDocumentSize}));
 app.use(express.static(path.join(__dirname, '../html')));
-
+app.use('/', indexRouter);
 app.use('/wopi', wopiRouter);
 
 module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+let express = require('express');
+let router = express.Router();
+let http = require('http');
+let https = require('https');
+let Dom = require('@xmldom/xmldom').DOMParser;
+let xpath = require('xpath');
+
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+router.get('/collaboraUrl', function(req, res) {
+    let collaboraOnlineHost = req.query.server;
+    let httpClient = collaboraOnlineHost.startsWith('https') ? https : http;
+    let data = '';
+    let request = httpClient.get(collaboraOnlineHost + '/hosting/discovery', function(response) {
+        response.on('data', function(chunk) { data += chunk.toString(); });
+        response.on('end', function() {
+            if (response.statusCode !== 200) {
+                let err = 'Request failed. Satus Code: ' + response.statusCode;
+                response.resume();
+                res.status(response.statusCode).send(err);
+                console.log(err)
+                return;
+            }
+            if (!response.complete) {
+                let err = 'No able to retrieve the discovery.xml file from the Collabora Online server with the submitted address.';
+                res.status(404).send(err);
+                console.log(err);
+                return;
+            }
+            let doc = new Dom().parseFromString(data);
+            if (!doc) {
+                let err = 'The retrieved discovery.xml file is not a valid XML file'
+                res.status(404).send(err)
+                console.log(err);
+                return;
+            }
+            let mimeType = 'text/plain';
+            let nodes = xpath.select("/wopi-discovery/net-zone/app[@name='" + mimeType + "']/action", doc);
+            if (!nodes || nodes.length !== 1) {
+                let err = 'The requested mime type is not handled'
+                res.status(404).send(err);
+                console.log(err);
+                return;
+            }
+            let onlineUrl = nodes[0].getAttribute('urlsrc');
+            res.json({
+                url: onlineUrl,
+                token: 'test'
+            });
+        });
+        response.on('error', function(err) {
+            res.status(404).send('Request error: ' + err);
+            console.log('Request error: ' + err.message);
+        });
+    });
+});
+
+
+module.exports = router;
